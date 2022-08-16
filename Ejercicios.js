dcbfd7dc2cb0911c2e0620e1101f99eb2dda9fdc
leono14 <109248892+leono14@users.noreply.github.com>
2022-08-16 13:40:55 -0300

    Update Ejercicios.js

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,13 +1,51 @@
-function Queue() {
+/*function Queue() {
     // Implementa la clase Queue
     // enqueue:   Agrega un valor a la queue.   Respeta el orden existente.
     // dequeue:   Remueve un valor de la queue.   Obedece a FIFO y respeta el underflow (cuando la queue tiene size cero, o sea, cuando no tiene ningún elemento).
     // size:   Devuelve el número de elementos que contiene la queue.
     this.array = [];
+}*/
+function Queue () {
+  this.first = null,
+  this.last = null,
+  this.size = 0
 }
+Queue.prototype.enqueue = function (lugar) {
+  const newNode = new Node (lugar);
+  if (!this.first) {
+    this.first = newNode,
+    this.last = newNode}
+  else {
+    this.last.next = newNode,
+    this.last = newNode
+  }
+  return ++this.size;
+}
+Queue.prototype.dequeue = function () {
+  if (!this.first) return null;
+  const ahora = this.first;
+  if (this.first === this.last) {
+    this.last = null
+  }
+  this.first = this.first.next;
+  this.size--;
+  return ahora.value
+}
+Queue.prototype.size = function () {
+  return this.size
+}
+const newQueue = new Queue ();
+newQueue.enqueue (1);
+newQueue.enqueue (2);
+newQueue.enqueue (3);
+newQueue.enqueue (4);
+newQueue.enqueue (5)
+console.log(newQueue.enqueue());
+console.log(newQueue.size)
 
 
 
+Me ganó este, paso.
 function cardGame() {
     // Implementar la función cardGame: a partir de dos Queue que va a recibir como paráemtros tiene
     // que determinar quién será el ganador del juego de cartas. Las reglas de dicho juego son las siguientes:
@@ -30,9 +68,34 @@ function cardGame() {
     //    - mazoUserB = [6,9,10,3,6,4]
 }
 
-function sets() {
+/*function sets() {
     // Implementa el metodo sets    
     // Usar el objeto set para obtener la cantidad de elementos iguales en dos arreglos
     // Muestra la cantidad
     // Aqui tu codigo
+}*/
+
+La lógica está bien, algo falla en la ejecución.
+
+Set.prototype.interseccion = function(nuevoSet) {
+  let setInterseccion = new Set();
+  for (const a of nuevoSet);
+    if (this.has(a)) {
+      setInterseccion.add(a)
+    }
+  return setInterseccion;
+}
+function sets(arr1, arr2) {
+  if (arr1 instanceof Array && arr2 instanceof Array) {
+    if (arr1.length && arr2.length) {
+      let set1 = new Set (arr1);
+      let set2 = new Set (arr2);
+      return set1.interseccion(set2).size > 0;
+    }
+  }
+}
+try {
+console.log(sets([25,88,209,311], [99,0,219,48] ));
+} catch (a) {
+    console.log("ERROR");
 }
